b7f2020fc71985306a71303b0315b89b7353a5e6
jack <s3707748@student.rmit.edu.au>
2019-08-06 16:26:14 +1000

    Initial Commit

---
+++ SimbaBirthdayDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimbaBirthdayDemo
+//
+//  Created by Jack Kelly on 6/8/19.
+//  Copyright © 2019 Jack Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
